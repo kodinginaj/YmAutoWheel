91d8faee121d28a867226363527db20b4b27bbff
henry <henrydwiseptian@gmail.com>
2020-06-15 21:14:34 +0700

    rapihin halaman ban

--- app/src/main/java/com/example/ymautowheel/api/Retroserver.java
+++ app/src/main/java/com/example/ymautowheel/api/Retroserver.java
@@ -4,8 +4,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Retroserver {
-    private static final String base_url = "http://192.168.100.4/YMautowheels/api/";
-    String url = "http://192.168.100.4/YMautowheels/api/";
+    private static final String base_url = "http://192.168.100.7/YMautowheels/api/";
+    String url = "http://192.168.100.7/YMautowheels/api/";
 
     private static Retrofit retrofit;
 
