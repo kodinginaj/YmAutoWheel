a6a41588f7084c8a64fcb431a3eb8ea270a8c42b
henry <henrydwiseptian@gmail.com>
2020-06-18 14:06:22 +0700

    update halaman velg

---
+++ app/src/main/java/com/example/ymautowheel/Informasi_stokvelg.java
@@ -0,0 +1,14 @@
+package com.example.ymautowheel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Informasi_stokvelg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informasi_stokvelg);
+    }
+}
